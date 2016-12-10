11ec24991ce587a6bab9f9a7802eda74bb030826
Adam Collins <adamcollinssecure@gmail.com>
2016-12-09 22:46:48 -0800

    first push to github

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	consumer_key:         'zZLpllwqPLPftOeOMoF53nN0O',
-	consumer_secret:      'bhzfbLiGgsT2FkZM91jNV1BeeMaoYN9z7pffQct9wOglLJtD0A',
-	access_token:         '517377422-mueMowTLLYjwD4avFilXsr9rbCKMnHRK46uILjMY',
-	access_token_secret:  'j1gggIllFLmxqs2A4YPv85UAcccpXHxVihfMyyTFeRqCJ',
-	bothandle: 'pyram66',
+	consumer_key:         '....',
+	consumer_secret:      '....',
+	access_token:         '....',
+	access_token_secret:  '....',
+	bothandle: '....',
 }
